6680dafe2b5da0e598878385f6d8f5d21f4a035f
Serdar Tas <i.serdar.tas@gmail.com>
2024-07-31 15:08:14 +0200

    content type testing

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 echo "This is a test script in the repository"
 echo "This line is added to test webhook trigger on github push event"
-echo "Test push"
+echo "Test #2 with url encoded content type"
