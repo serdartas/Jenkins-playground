545078886678b8e64c960aea2996cef463044d1a
Serdar Tas <i.serdar.tas@gmail.com>
2024-07-31 15:40:31 +0200

    test webhook

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 echo "This is a test script in the repository"
 echo "This line is added to test webhook trigger on github push event"
-echo "Test #3 with url encoded content type"
+echo "Test #4: Fixed the wront IP address in webhook link"
